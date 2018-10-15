d800b0a3d9ce92754f14d42ca6957a4adfd06a96
student <student@pc-208-03>
2018-10-15 11:13:29 +0200

    Cwiczenia 15.10.2018

--- src/Cw_15_10/cwicz1.java
+++ src/Cw_15_10/cwicz1.java
@@ -14,7 +14,7 @@ public class cwicz1 {
     public static void main(String[] args) {
             //Podzbiory2
             
-            program permutacji str 21 - przeanalizowac
+            //program permutacji str 21 - przeanalizowac
             
     }
 
